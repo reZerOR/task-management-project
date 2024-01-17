2ad0768983afeaebb5e3281ce4f9fe8be3d4248a
Coder Emran <coderemran6910@gmail.com>
2024-01-17 23:31:30 +0600

    added new brunch

--- src/pages/Home/components/Banner.tsx
+++ src/pages/Home/components/Banner.tsx
@@ -1,5 +1,14 @@
 const Banner = () => {
-  return <div>Banner</div>;
+  return 
+  <div>
+    {/* Left side */}
+    <div> 
+      <h1> Unlock the power of unified teamwork with TaskFlow's Team Management Hub. Our platform is designed to streamline communication, boost productivity, and foster a collaborative environment for your team. </h1>
+    </div>
+
+    {/* Right side  */}
+    <div></div>
+  </div>;
 };
 
 export default Banner;
