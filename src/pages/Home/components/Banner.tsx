3f3ed3dcb6aaf39a84d082de9685717f6189f230
Coder Emran <coderemran6910@gmail.com>
2024-01-18 16:36:18 +0600

    Add Home page banner responsive code

--- src/pages/Home/components/Banner.tsx
+++ src/pages/Home/components/Banner.tsx
@@ -10,12 +10,67 @@ const Banner = () => {
 
   return (
     <div className=" bg-[#F4F6F6]">
-      <div className="container mx-auto flex justify-between items-center  h-[80vh]">
+      <div className="container mx-auto flex flex-col xl:flex-row justify-between items-center h-auto xl:h-[80vh] ">
         {/* Left side */}
         <div className="flex-1 mr-12 ">
           {/* reviews */}
-          <div className="flex justify-between  mb-20 ">
-            <figure className="max-w-screen-md">
+          <div className="flex justify-between  mb-10 ">
+            <figure className="max-w-screen-md flex flex-col justify-center items-center mx-auto xl:mx-0  mt-2 xl:block">
+              <div className="flex items-center mb-4 text-yellow-300">
+                <svg
+                  className="w-5 h-5 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                  className="w-5 h-5 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                  className="w-5 h-5 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                  className="w-5 h-5 me-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              </div>
+              <blockquote>
+                <p className="text-sm font-semibold text-gray-500 dark:text-white">
+                  "This tools is awesome and I love it."
+                </p>
+              </blockquote>
+            </figure>
+
+            <figure className="max-w-screen-md hidden xl:block">
               <div className="flex items-center mb-4 text-yellow-300">
                 <svg
                   className="w-5 h-5 me-1"
@@ -70,62 +125,7 @@ const Banner = () => {
               </blockquote>
             </figure>
 
-            <figure className="max-w-screen-md">
-              <div className="flex items-center mb-4 text-yellow-300">
-                <svg
-                  className="w-5 h-5 me-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-5 h-5 me-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-5 h-5 me-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-5 h-5 me-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-              </div>
-              <blockquote>
-                <p className="text-sm font-semibold text-gray-500 dark:text-white">
-                  "This tools is awesome "
-                </p>
-              </blockquote>
-            </figure>
-
-            <figure className="max-w-screen-md">
+            <figure className="max-w-screen-md hidden xl:block">
               <div className="flex items-center mb-4 text-yellow-300">
                 <svg
                   className="w-5 h-5 me-1"
@@ -181,33 +181,31 @@ const Banner = () => {
             </figure>
           </div>
 
-          <h1 className=" text-5xl font-extrabold text-gray-800  leading-snug">
+          <h1 className=" text-3xl text-center lg:text-left xl:text-5xl font-extrabold text-gray-800  leading-snug">
             Unlock the power of unified teamwork with{" "}
             <span className=" text-primeColor">TaskFlow's </span> Team
             Management Hub.
           </h1>
 
-          <div className="mt-12">
-            <p>
+          <div className="mt-12 px-10 xl:px-0">
+            <p >
               <strong style={gradientBorder} className="border-b-2">
-                {" "}
                 Earlier:
-              </strong>{" "}
+              </strong>
               "Embrace the power of productivity. Yesterday is gone, but with
               TaskFlow, every 'Earlier' paves the way for tomorrow's success."
             </p>
             <p className="mt-5">
               <strong style={gradientBorder} className="border-b-2">
-                {" "}
                 With TaskFlow's:
-              </strong>{" "}
+              </strong>
               "Empower your team with TaskFlow: where 'Earlier' is a stepping
               stone and every task is a stride towards seamless collaboration
               and project success."
             </p>
           </div>
 
-          <div className="mt-12 flex gap-48 ">
+          <div className="mt-12 flex flex-col lg:flex-row gap-20 md:gap-48 items-center mb-10 ">
             <button className="text-xl  px-4 h-16 before:block before:absolute hover:before:bg-primeColor before:w-0 before:h-0 hover:before:h-20 hover:before:w-full before:-bottom-2 before:right-0 before:duration-500 before:rounded-xl before:-z-10 relative inline-block transform hover:text-white text-secondColor bg-transparent border-2 overflow-hidden border-sky-700 duration-500">
               Start your free trial
             </button>
