391bb910d1aff8a5c7012f94b1a3174d5a5b69f7
Coder Emran <coderemran6910@gmail.com>
2024-01-25 07:52:40 +0600

    added 'create project' UI and '/tasksboard' router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,7 @@ import Main from "../layouts/Main";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import TasksBoard from "../pages/TasksBoard/TaskBoard";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register></Register>
       },
+      {
+        path: "/tasksboard",
+        element: <TasksBoard />
+      }
     ],
   },
 ]);
