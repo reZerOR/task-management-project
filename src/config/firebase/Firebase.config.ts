c7c124d4e7857a824978d8c291f3b739fc3e746e
reZerOR <gkraihan14k@gmail.com>
2024-01-25 22:26:26 +0600

    env updated

--- src/config/firebase/Firebase.config.ts
+++ src/config/firebase/Firebase.config.ts
@@ -4,18 +4,18 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-import { getAuth,Auth} from "firebase/auth";
+import { getAuth, Auth } from "firebase/auth";
 const firebaseConfig = {
-    apiKey:import.meta.env.VITE_apiKey,
-    authDomain:import.meta.env.VITE_authDomain,
-    projectId:import.meta.env.VITE_projectId,
-    storageBucket:import.meta.env.VITE_storageBucket,
-    messagingSenderId:import.meta.env.VITE_messagingSenderId,
-    appId:import.meta.env.VITE_appId 
+  apiKey: process.env.VITE_apiKey,
+  authDomain: process.env.VITE_authDomain,
+  projectId: process.env.VITE_projectId,
+  storageBucket: process.env.VITE_storageBucket,
+  messagingSenderId: process.env.VITE_messagingSenderId,
+  appId: process.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth: Auth= getAuth(app);
+const auth: Auth = getAuth(app);
 
 export default auth;
