5a19bba973ebf71526c68e299e8667086e63f38e
Coder Emran <coderemran6910@gmail.com>
2024-01-26 19:17:14 +0600

    improve TaskBord UI Design

--- src/pages/TasksBoard/SingleTask.tsx
+++ src/pages/TasksBoard/SingleTask.tsx
@@ -1,16 +1,19 @@
+import { MdOutlineVisibility } from "react-icons/md";
+
 const SingleTask = ({
   task,
 }: {
   task: { name: string; description: string; visibility: string };
 }) => {
-  console.log(task);
+
   const { name, description, visibility } = task;
 
   return (
-    <div className="bg-slate-100 p-5">
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>{visibility}</p>
+    <div className="bg-slate-100 p-5  border rounded-xl m-2 w-full ">
+      <h3 className=" font-bold">{name}</h3>
+      <hr />
+      <p className="text-slate-500">{description}</p>
+      <p className="text-sm text-slate-400 mt-5 flex  items-center "> <MdOutlineVisibility className="text-2xl " /> {visibility}</p>
     </div>
   );
 };
