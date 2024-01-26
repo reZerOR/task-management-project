b4f54ff44409c77ed88d2453d4d55a6990a7da72
reZerOR <gkraihan14k@gmail.com>
2024-01-26 12:06:32 +0600

    types resolved in singltask

--- src/pages/TasksBoard/SingleTask.tsx
+++ src/pages/TasksBoard/SingleTask.tsx
@@ -1,14 +1,18 @@
-const SingleTask = ({task}) => {
-    console.log(task);
-    const {name, description, visibility} = task
-    
-    return (
-        <div className="bg-slate-100 p-5">
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <p>{visibility}</p>
-        </div>
-    );
+const SingleTask = ({
+  task,
+}: {
+  task: { name: string; description: string; visibility: string };
+}) => {
+  console.log(task);
+  const { name, description, visibility } = task;
+
+  return (
+    <div className="bg-slate-100 p-5">
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <p>{visibility}</p>
+    </div>
+  );
 };
 
-export default SingleTask
+export default SingleTask;
