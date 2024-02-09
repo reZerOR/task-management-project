2a1592133f55f7d6b37ee0117cf2dc26f657f509
Salman-080 <salman.niloy552@gmail.com>
2024-02-09 19:40:52 +0600

    user profile polishing and register imagebbapi used

--- src/Hooks/UserInfo/useUserInfo.tsx
+++ src/Hooks/UserInfo/useUserInfo.tsx
@@ -1,22 +1,30 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { useContext } from "react";
-// import { AuthContext } from "../../Providers/AuthProvider";
-// import useAxiosPrivate from "../AxiosPrivate/useAxiosPrivate";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
+import useAxiosPrivate from "../AxiosPrivate/useAxiosPrivate";
 
-// const useUserInfo = () => {
-// //     const {user}=useContext(AuthContext);
-// //     const axiosPrivate= useAxiosPrivate();
+const useUserInfo = () => {
+    const { user } = useContext(AuthContext);
+    const axiosPrivate = useAxiosPrivate();
 
-// //   const {data: userInfo, refetch}= useQuery({
-// //     queryKey: ["userInfo", user?.email],
-// //     queryFn: async ()=>{
-// //         const res= await axiosPrivate.get(`/currentUserInfo/${user?.email}`);
-// //         console.log(res.data)
-// //         return res.data;
-// //     }
-// //   })
+    const { data: userInfo = {}, refetch } = useQuery({
+        queryKey: ["userInfo", user?.email],
+        queryFn: async () => {
+            if (!user || !user.email){
+                return {};
+            } 
+            const res = await axiosPrivate.get(`/currentUserInfo/${user?.email}`);
+            if (res.data) {
+                return res.data;
+            }
+            else {
+                return {};
+            }
 
-// //   return [userInfo,refetch];
-// };
+        }
+    })
 
-// export default useUserInfo;
+    return [userInfo, refetch];
+};
+
+export default useUserInfo;
--- src/pages/MyProfile/MyProfile.tsx
+++ src/pages/MyProfile/MyProfile.tsx
@@ -1,13 +1,21 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import useAxiosPrivate from "../../Hooks/AxiosPrivate/useAxiosPrivate";
 // import useUserInfo from "../../Hooks/UserInfo/useUserInfo";
 import { AuthContext } from "../../Providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 import { ToastContainer, toast } from "react-toastify";
+import useAxiosPublic from "../../Hooks/AxiosPublic/useAxiosPublic";
+const image_hosting_key = import.meta.env.VITE_IMAGE_API;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+import { MdOutlineDeleteForever } from "react-icons/md";
+import Swal from "sweetalert2";
+
 
 const MyProfile = () => {
   // const [userInfo, refetch] = useUserInfo();
   const axiosPrivate = useAxiosPrivate();
+  const axiosPublic = useAxiosPublic();
+  const imageInputRef = useRef<HTMLInputElement>(null);
   // console.log(userInfo)
   const { user } = useContext(AuthContext);
 
@@ -24,79 +32,192 @@ const MyProfile = () => {
 
   const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const userName = (
-      e.currentTarget.elements.namedItem("name") as HTMLInputElement
-    ).value;
-    const userImage = (
-      e.currentTarget.elements.namedItem("image") as HTMLInputElement
-    ).value;
 
-    console.log(userName, userImage);
-    const UpdateUserInfo = {
-      userName: userName,
-      userImage: userImage,
-    };
+    const form = e.target as HTMLFormElement;
+    
 
-    const res = await axiosPrivate.put(
-      `/updateUserInfo/${userInfo.email}`,
-      UpdateUserInfo
-    );
-    console.log(res.data);
-    if (res.data.modifiedCount > 0) {
-      refetch();
-      toast.success("Profile updated successfully");
+    const imageFile = {
+      image: form.image.files[0],
+
     }
+
+
+    if (!imageFile.image) {
+      const userName = (form.elements.namedItem("name") as HTMLInputElement)?.value;
+
+      // console.log(userName,"success")
+
+      console.log(userName);
+      const UpdateUserInfo = {
+        userName: userName,
+
+      };
+      console.log(UpdateUserInfo)
+
+      const res = await axiosPrivate.put(
+        `/updateUserInfo/${userInfo.email}`,
+        UpdateUserInfo
+      );
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        if (imageInputRef.current) {
+          imageInputRef.current.value = '';
+        }
+        toast.success("Profile updated successfully");
+      }
+    }
+    else {
+
+      const result = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      });
+
+      if (result.data.success) {
+        const userName = (form.elements.namedItem("name") as HTMLInputElement)?.value;
+
+        // console.log(userName,"success")
+        const userImage = result.data.data.display_url;
+
+        console.log(userName, userImage);
+        const UpdateUserInfo = {
+          userName: userName,
+          userImage: userImage,
+        };
+        console.log(UpdateUserInfo)
+
+        const res = await axiosPrivate.put(
+          `/updateUserInfo/${userInfo.email}`,
+          UpdateUserInfo
+        );
+        console.log(res.data);
+        if (res.data.modifiedCount > 0) {
+          refetch();
+          if (imageInputRef.current) {
+            imageInputRef.current.value = '';
+          }
+          toast.success("Profile updated successfully");
+        }
+      }
+
+    }
+
+
+
+
   };
+
+  const handleRemovePhoto = () => {
+    Swal.fire({
+      title: "Are You sure that You want to Remove your Photo?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Save",
+      denyButtonText: `Cancel`
+    }).then(async (result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        const res = await axiosPublic.patch(`/userProfile/removePhoto/${userInfo?._id}`);
+        if (res?.data?.modifiedCount > 0) {
+          refetch();
+          Swal.fire("Saved!", "", "success");
+        }
+
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  }
   return (
-    <div className="min-h-[calc(100vh-348px)] mb-12 lg:mb-0">
-      <form
-        onSubmit={handleUpdate}
-        className="bg-primeColor bg-opacity-35 md:w-[450px] rounded-lg  mx-auto mt-12 px-5 py-7 space-y-2"
-      >
-        <div className="flex flex-col gap-1">
-          <h2 className="text-lg">Full Name: </h2>
+    <div className="min-h-[calc(100vh-348px)] mb-12 lg:mb-6">
 
-          <input
-            className="py-3 px-3 rounded-lg"
-            defaultValue={userInfo?.displayName}
-            type="text"
-            name="name"
-            id=""
-          />
+      <div className="w-[400px] md:w-[600px]  mx-auto bg-primeColor bg-opacity-35 mt-14 py-6 rounded-2xl">
+
+        <div className="space-y-4">
+
+          <div className="relative group">
+
+            <div className="h-[200px] w-[200px] mx-auto rounded-full border-4 border-black group-hover:brightness-50 ">
+              <img className="rounded-full h-full w-full hover:scale-110 " src={userInfo?.photoURL ? userInfo.photoURL : 'https://i.ibb.co/yQkyd9m/no-user-image-icon-10.png'} alt="" />
+
+            </div>
+            {userInfo?.photoURL && (
+              <MdOutlineDeleteForever
+                onClick={handleRemovePhoto}
+                className="bg-red-500 py-0 px-0 border border-red-500 rounded shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 hover:bg-red-500 h-[60px] w-[60px] text-white cursor-pointer"
+              />
+            )}
+            
+
+
+          </div>
+
+
+
+          <div className="text-center">
+            <h2 className="text-3xl font-bold">{userInfo?.displayName}</h2>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-1">
-          <h2 className="text-lg">Image </h2>
 
-          <input
-            className="py-3 px-3 rounded-lg"
-            defaultValue={userInfo?.photoURL}
-            type="text"
-            name="image"
-            id=""
-          />
-        </div>
+        {/* todo */}
 
-        <div className="flex flex-col gap-1">
-          <h2 className="text-lg">Email: </h2>
+        <form
+          onSubmit={handleUpdate}
+          className="  rounded-lg  mx-auto mt-2 px-5 py-7 space-y-6"
+        >
+          <div className="flex flex-col gap-1">
+            <h2 className="text-lg font-semibold">Full Name: </h2>
 
-          <input
-            className="bg-gray-200 py-3 px-3 rounded-lg cursor-not-allowed"
-            defaultValue={userInfo?.email}
-            readOnly
-            type="text"
-            name=""
-            id=""
-          />
-        </div>
+            <input
+              className="py-3 px-3 rounded-lg"
+              defaultValue={userInfo?.displayName}
+              type="text"
+              name="name"
+              id=""
+            />
+          </div>
 
-        <br />
-        <div className="text-center">
-          <button className="px-8 rounded-lg py-3 bg-primeColor text-white ">
-            Update
-          </button>
-        </div>
-      </form>
+          <div className="flex flex-col gap-1">
+            <h2 className="text-lg font-semibold">Email: </h2>
+
+            <input
+              className="bg-gray-200 py-3 px-3 rounded-lg cursor-not-allowed"
+              defaultValue={userInfo?.email}
+              readOnly
+              type="text"
+              name=""
+              id=""
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <h2 className="text-lg font-semibold">Upload Image</h2>
+
+            <input
+              className="py-1 rounded-lg"
+              ref={imageInputRef}
+              type="file"
+              name="image"
+              id=""
+            />
+          </div>
+
+          <br />
+          <div className="text-center">
+            <button className="px-8 rounded-lg py-3 bg-primeColor text-white ">
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+
+
+
+
+
       <ToastContainer></ToastContainer>
     </div>
   );
